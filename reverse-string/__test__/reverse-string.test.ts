b15323d3ac148f04f0c3369415045390131b7e6a
Slobodan Dan <danastroman@gmail.com>
2023-06-21 11:19:22 +0200

    Reverse string implementation.

--- reverse-string/__test__/reverse-string.test.ts
+++ reverse-string/__test__/reverse-string.test.ts
@@ -2,11 +2,11 @@ import { reverse } from './reverse-string';
 
 it('Returns a correctly reversed string', () => {
     const apple = reverse('apple');
-    expect(apple).toEqual('leppa');
+    expect(apple).toEqual('elppa');
 
     const hello = reverse('hello');
     expect(hello).toEqual('olleh');
 
     const greetings = reverse('Greetings!');
-    expect(greetings).toEqual('!sgnteerG');
+    expect(greetings).toEqual('!sgniteerG');
 });
--- reverse-string/__test__/reverse-string.ts
+++ reverse-string/__test__/reverse-string.ts
@@ -6,5 +6,29 @@
 // reverse('Greetings!') === '!sgnteerG
 
 export const reverse = (str: string) => {
-    return 'leppa';
+    return str.split('').reduce((reversed, char) => char + reversed, '');
 };
+
+// solution #1
+// export const reverse = (str: string) => {
+//     return str.split('').reverse().join('');
+// };
+
+// solution #2
+// export const reverse = (str: string) => {
+//     let reversed = '';
+//     for (let character of str) reversed = character + reversed;
+//     return reversed;
+// };
+
+// solution #3
+// export const reverse = (str: string) => {
+//     return str.split('').reduce((reversed, char) => char + reversed, '');
+// };
+
+// solution #4
+// export const reverse = (str: string) => {
+//     const result = [];
+//     for (let i = str.length; i >= 0; i--) result.push(str.charAt(i));
+//     return result.join('');
+// };
